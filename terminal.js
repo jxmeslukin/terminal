14db7ebd4c86ad87edb25681715844576e16a716
jxmeslukin <jimmy.lukin@gmail.com>
2021-03-04 15:02:32 +1100

    v1

---
+++ terminal.js
@@ -0,0 +1,11 @@
+// In body tag
+$('body').terminal({
+    iam: function (name) {
+        this.echo('Hello, ' + name +
+            '. Welcome to GeeksForGeeks');
+    }
+}, {
+    greetings: 'GeeksForGeeks - A place to'
+            + ' learn DS, Algo and Computer'
+            + ' Science for free'
+});
